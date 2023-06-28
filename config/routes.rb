d3e7b770f7f6039a3c775bae992f53b4a47eab10
colleenrust <colleenrust524@gmail.com>
2023-06-27 20:02:19 -0500

    authentication full-stack style and inital commit major slay

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+end
